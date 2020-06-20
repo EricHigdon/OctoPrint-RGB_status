d54a55dea925cf77c70f06d597537c9397ff847f
Eric Higdon <EricHigdon@users.noreply.github.com>
2020-06-20 11:08:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, unicode_literals
 
 ########################################################################################################################
 ### Do not forget to adjust the following variables to your own plugin.
